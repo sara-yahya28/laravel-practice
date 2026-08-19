4eeb88c98f5fa45a2023fa7e77befe1cd6d23303
Sara <sara.yahyamqbl@gmail.com>
2026-08-19 11:10:06 +0300

    refactor: remove BlogController, Blog model, and related migrations and views

--- resources/views/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-{{-- <x-fileName> -> when this strutcure is seen,
-    automatically search for components folder --}}
-
-{{-- if file is not, use @yield() + @extent() --}}
-<x-layout title="Welcome">
-
-<h1>Welcome Page</h1>
-
-</x-layout>
