f8bbc704c95a6d015287a428b855760a6dff517e
Sara Yahya <sara.yahyamqbl@gmail.com>
2026-08-22 18:58:54 +0300

    Delete resources/views/partials directory

--- resources/views/partials/navbar2.blade.php
+++
@@ -1,6 +0,0 @@
-<ul class="navbar">
-<li><a href="/">Home</a></li>
-<li><a href="{{route('product')}}">Product</a></li>
-<li><a href="/blogs">Blog</a></li>
-<li><a href="{{route('createBlog')}}">Create Blog</a></li>
-</ul>
--- resources/views/partials/sidebar.blade.php
+++
@@ -1,3 +0,0 @@
-@section('sidebar')
-<div class="container">This Is Sidebar From <span class="color">Template Layout</span></div>
- @show{{--create section and yield at same time --}}
