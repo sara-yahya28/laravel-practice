db236318ddf675f86a0559b5afeaa53c36977c1b
Sara <sara.yahyamqbl@gmail.com>
2026-08-22 15:52:01 +0300

    feat(i18n): add Arabic translations and RTL support

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,13 +13,10 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('لوحة التحكم') }}
                     </x-nav-link>
-                </div>
-            </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('posts.index')" :active="request()->routeIs('dashboard')">
-                        {{ __('Posts') }}
+                    <x-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.*')">
+                        {{ __('مقالات') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -74,6 +71,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.*')">
+                {{ __('Posts') }}
             </x-responsive-nav-link>
         </div>
 
